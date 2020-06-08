598b56149d25940ee1abec39d6c40952925d1390
Duke Ko <dukhyunko93@gmail.com>
2020-06-08 20:26:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,10 +79,7 @@ programmer_hash =
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable.
-  # return the entire updated hash
-
-	programmer_hash =
+  programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
